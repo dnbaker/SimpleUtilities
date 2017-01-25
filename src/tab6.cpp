afbe0b5d07e6793528cb6affa4d7329ba36f90cf
dnbh <dnb@cs.jhu.edu>
2017-01-25 16:41:41 -0500

    Add streaming option for interleaved fastq.

--- src/tab6.cpp
+++ src/tab6.cpp
@@ -11,22 +11,62 @@ KSEQ_INIT(gzFile, gzread)
         kputsn(suf, sizeof(suf), &kseq->name);\
     } while(0)
 
+int streaming_main(char *outpath) {
+    FILE *out(outpath ? fopen(outpath, "w"): stdout);
+    gzFile in(gzdopen(STDIN_FILENO, "rb"));
+    kseq_t *ks(kseq_init(in));
+    kstring_t s{0, 0, nullptr};
+    int code(EXIT_SUCCESS);
+    for(;;) {
+        if(kseq_read(ks) < 0) break;
+        s.l = 0;
+        __add_if_abs(ks, "/1");
+        kputsn(ks->name.s, ks->name.l, &s);
+        s.s[s.l++] = '\t';
+        kputsn(ks->seq.s,  ks->seq.l,  &s);
+        s.s[s.l++] = '\t';
+        kputsn(ks->qual.s, ks->qual.l, &s);
+        s.s[s.l++] = '\t';
+        if(kseq_read(ks) < 0) {
+            std::fprintf(stderr, "Uneven number of reads.\n");
+            code = EXIT_FAILURE;
+        }
+        __add_if_abs(ks, "/2");
+        kputsn(ks->name.s, ks->name.l, &s);
+        s.s[s.l++] = '\t';
+        kputsn(ks->seq.s, ks->seq.l, &s);
+        s.s[s.l++] = '\t';
+        kputsn(ks->qual.s, ks->qual.l, &s);
+        s.s[s.l++] = '\n';
+        s.s[s.l] = '\0';
+        fwrite(s.s, 1uL, s.l, out);
+    }
+    kseq_destroy(ks);
+    free(s.s);
+    if(out != stdout) fclose(out);
+    return code;
+}
+
 
 int main(int argc, char *argv[])
 {
-    int c;
+    int c, streaming(0);
     char *outpath(nullptr);
-    if(argc < 2) {
-        usage:
-        std::fprintf(stderr,"Function: Converts paired-end sequencing data into tab6 format for consumption by bowtie2\n" 
-                       "Usage: %s <r1> <r2>\nFlags:\n-o\tEmit to file instead of stdout.\n", *argv);
-        exit(EXIT_FAILURE);
-    }
-    while((c = getopt(argc, argv, "o:h?")) >= 0) {
+    while((c = getopt(argc, argv, "o:sh?")) >= 0) {
         switch(c) {
             case 'h': case '?': goto usage;
             case 'o': outpath = optarg; break;
+            case 's': streaming = 1; break;
         }
+    }
+    if(streaming) return streaming_main(outpath);
+    if(argc - optind < 2) {
+        usage:
+        std::fprintf(stderr,"Function: Converts paired-end sequencing data into tab6 format for consumption by bowtie2\n" 
+                            "Usage: %s <r1> <r2>\nFlags:\n-o\tEmit to file instead of stdout.\n"
+                            "-s\tUse in streaming fashion (reads from stdin, emits to stdout by default).\n",
+                     *argv);
+        exit(EXIT_FAILURE);
     }
     FILE *out(outpath ? fopen(outpath, "w"): stdout);
     gzFile in1(gzopen(argv[optind], "rb"));
